ecd3dbe3bddab963baae08229ac5b7a438498136
engcfraposo <engcfraposo@gmail.com>
2020-05-19 22:57:16 -0300

    update 19/05/2020

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes'
-import Login from './components/login'
-import Journal from './components/journal'
-import University from './components/university'
-import Whoweare from './components/about'
-import Navbars from './components/navbar'
 import './App.css';
 
 ReactDOM.render(
